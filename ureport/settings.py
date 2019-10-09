11f801f52828235c14a8a7707ec8beda7a3d962d
Eduardo Douglas <edudouglas@gmail.com>
2019-10-09 17:56:11 -0300

    Added settings.py

---
+++ ureport/settings.py
@@ -0,0 +1,188 @@
+import dj_database_url
+import sentry_sdk
+
+from decouple import config
+from sentry_sdk.integrations.django import DjangoIntegration
+from ureport.settings_common import *  # noqa
+
+
+DEBUG = config("DEBUG", default=False, cast=bool)
+TEMPLATE_DEBUG = DEBUG
+
+EMPTY_SUBDOMAIN_HOST = config("EMPTY_SUBDOMAIN_HOST", "http://localhost:8000")
+HOSTNAME = config("APP_HOSTNAME", "localhost:8000")
+SITE_HOST_PATTERN = config("SITE_HOST_PATTERN", default="http://{}.localhost:8000")
+
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*", cast=lambda v: [s.strip() for s in v.split(",")])
+
+# SESSION_COOKIE_DOMAIN = config("SESSION_COOKIE_DOMAIN", "localhost:8000")
+SESSION_COOKIE_SECURE = False
+
+ADMINS = config(
+    "ADMINS",
+    default="Ilhasoft|contato@ilhasoft.com.br",
+    cast=lambda v: [(s.strip().split("|")[0], s.strip().split("|")[1]) for s in v.split(",")] if v else [],
+)
+MANAGERS = ADMINS
+
+TIME_ZONE = config("TIME_ZONE", default="America/Sao_Paulo")
+USER_TIME_ZONE = config("USER_TIME_ZONE", default="America/Sao_Paulo")
+
+EMAIL_HOST = config("EMAIL_HOST", default="localhost")
+EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", "no-reply@ilhasoft.com.br")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="no-reply@ilhasoft.com.br")
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID", default="")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY", default="")
+AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME", default="")
+
+AWS_S3_SECURE_URLS = False
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_FILE_OVERWRITE = False
+
+MEDIA_URL = config("MEDIA_URL", default="http://localhost:8000/media/")
+SITE_API_HOST = config("SITE_API_HOST", default="https://rapidpro.ilhasoft.mobi")
+API_ENDPOINT = config("SITE_API_HOST", default="https://rapidpro.ilhasoft.mobi")
+
+DATABASES = {}
+DATABASES["default"] = dj_database_url.parse(config("DEFAULT_DATABASE"))
+DATABASES["default"]["CONN_MAX_AGE"] = 0
+
+MIDDLEWARE = MIDDLEWARE + ("whitenoise.middleware.WhiteNoiseMiddleware",)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "HTTPS")
+
+REDIS_HOST = config("REDIS_HOST", default="localhost")
+REDIS_DATABASE = config("REDIS_DATABASE", default="1")
+
+BROKER_URL = "redis://{}:6379/{}".format(REDIS_HOST, REDIS_DATABASE)
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": BROKER_URL,
+        "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
+    }
+}
+
+CELERY_RESULT_BACKEND = config("CELERY_RESULT_BACKEND", default=BROKER_URL)
+CELERY_ALWAYS_EAGER = config("CELERY_ALWAYS_EAGER", default=False, cast=bool)
+CELERY_EAGER_PROPAGATES_EXCEPTIONS = config("CELERY_EAGER_PROPAGATES_EXCEPTIONS", default=True, cast=bool)
+
+COMPRESS_ENABLED = config("COMPRESS_ENABLED", default=False, cast=bool)
+COMPRESS_OFFLINE = config("COMPRESS_OFFLINE", default=False, cast=bool)
+COMPRESS_CSS_FILTERS = ["compressor.filters.css_default.CssAbsoluteFilter", "compressor.filters.cssmin.CSSMinFilter"]
+COMPRESS_JS_FILTERS = ["compressor.filters.jsmin.JSMinFilter"]
+
+sentry_sdk.init(dsn=config("RAVEN_CONFIG", default=""), integrations=[DjangoIntegration()])
+
+OAUTHLIB_CLIENT_ID = config("OAUTHLIB_CLIENT_ID", default="")
+OAUTHLIB_SECRET = config("OAUTHLIB_SECRET", default="")
+OAUTHLIB_AUTHORIZATION_URL = config(
+    "OAUTHLIB_AUTHORIZATION_URL", default="http://dataforallcloud.org/oauth2/authorize"
+)
+OAUTHLIB_TOKEN_URL = config("OAUTHLIB_TOKEN_URL", default="http://dataforallcloud.org/oauth2/token")
+OAUTHLIB_INSECURE_TRANSPORT = config("OAUTHLIB_INSECURE_TRANSPORT", default=False)
+OAUTHLIB_REDIRECT_URI = config("OAUTHLIB_REDIRECT_URI", default="https://rtm-ilhasoft.ngrok.io/callback")
+OAUTHLIB_USER_URL = config("OAUTHLIB_USER_URL", default="http://dataforallcloud.org/oauth2/userInfo")
+OAUTHLIB_MOMSERVICE_TOKEN_URL = config(
+    "OAUTHLIB_MOMSERVICE_TOKEN_URL", default="http://dataforallcloud.org/monservice/api/v1/token"
+)
+OAUTHLIB_MOMSERVICE_USER_URL = config(
+    "OAUTHLIB_MOMSERVICE_USER_URL", default="http://dataforallcloud.org/monservice/api/v1/rtmUserInfo"
+)
+OAUTHLIB_APP_ID = config("OAUTHLIB_APP_ID", default="uninfortm")
+
+# custom configs
+INSTALLED_APPS += (
+    "sass_processor",
+    "qurl_templatetag",
+    "ureport.uncts",
+    "ureport.accounts",
+)
+
+SITE_ALLOW_NO_ORG += (
+    "uncts.unct_create",
+    "uncts.unct_list",
+    "uncts.unct_update",
+    "accounts.user_org_list",
+    "accounts.user_org_create",
+    "accounts.user_org_update",
+    "accounts.user_org_delete",
+)
+
+STATICFILES_FINDERS += (
+    "sass_processor.finders.CssFinder",
+)
+
+SASS_PROCESSOR_INCLUDE_FILE_PATTERN = r"^.+\.scss$"
+
+LOGIN_URL = "/users/login/"
+LOGOUT_URL = "/users/logout/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
+SITE_CHOOSER_TEMPLATE = "public/index.html"
+SITE_CHOOSER_URL_NAME = "public.index"
+
+## ONLY DEV
+CELERYBEAT_SCHEDULE = {
+    "refresh_flows": {"task": "polls.refresh_org_flows", "schedule": timedelta(minutes=1), "relative": True},
+    "recheck_poll_flow_data": {
+        "task": "polls.recheck_poll_flow_data",
+        "schedule": timedelta(minutes=1),
+        "relative": True,
+    },
+    "fetch_old_sites_count": {
+        "task": "polls.fetch_old_sites_count",
+        "schedule": timedelta(minutes=1),
+        "relative": True,
+    },
+    "contact-pull": {
+        "task": "dash.orgs.tasks.trigger_org_task",
+        "schedule": timedelta(minutes=1),
+        "args": ("ureport.contacts.tasks.pull_contacts",),
+    },
+    "backfill-poll-results": {
+        "task": "dash.orgs.tasks.trigger_org_task",
+        "schedule": timedelta(minutes=1),
+        "relative": True,
+        "args": ("ureport.polls.tasks.backfill_poll_results", "sync"),
+    },
+    "results-pull-main-poll": {
+        "task": "dash.orgs.tasks.trigger_org_task",
+        "schedule": timedelta(minutes=1),
+        "args": ("ureport.polls.tasks.pull_results_main_poll", "sync"),
+    },
+    "results-pull-recent-polls": {
+        "task": "dash.orgs.tasks.trigger_org_task",
+        "schedule": timedelta(minutes=1),
+        "relative": True,
+        "args": ("ureport.polls.tasks.pull_results_recent_polls", "sync"),
+    },
+    "results-pull-brick-polls": {
+        "task": "dash.orgs.tasks.trigger_org_task",
+        "schedule": timedelta(minutes=1),
+        "relative": True,
+        "args": ("ureport.polls.tasks.pull_results_brick_polls", "sync"),
+    },
+    "results-pull-other-polls": {
+        "task": "dash.orgs.tasks.trigger_org_task",
+        "schedule": timedelta(minutes=1),
+        "relative": True,
+        "args": ("ureport.polls.tasks.pull_results_other_polls", "sync"),
+    },
+    "refresh-engagement-data": {
+        "task": "dash.orgs.tasks.trigger_org_task",
+        "schedule": timedelta(minutes=1),
+        "relative": True,
+        "args": ("ureport.stats.tasks.refresh_engagement_data", "sync"),
+    },
+}
