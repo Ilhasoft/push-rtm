4900569fc72ff52bd1e872c0497f5e61af1c0565
Eduardo Douglas <edudouglas@gmail.com>
2019-10-10 17:03:11 -0300

    word cloud chart

--- ureport/results/views.py
+++ ureport/results/views.py
@@ -1,10 +1,42 @@
-from smartmin.views import SmartTemplateView
+import json
+
+from django.http import HttpResponse
+
+from smartmin.views import SmartReadView
+
+from ureport.polls.models import Poll, PollQuestion
 
 
-class ResultPollView(SmartTemplateView):
+class PollReadView(SmartReadView):
     template_name = "results/poll_result.html"
     permission = "polls.poll_list"
+    model = Poll
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["org"] = self.request.org
+        context["poll"] = self.get_object()
         return context
+
+    def derive_queryset(self):
+        queryset = super(PollReadView, self).derive_queryset()
+        queryset = queryset.filter(org=self.request.org, is_active=True, has_synced=True)
+        return queryset
+
+
+class PollQuestionResultsView(SmartReadView):
+    model = PollQuestion
+
+    def derive_queryset(self):
+        queryset = super(PollQuestionResultsView, self).derive_queryset()
+        queryset = queryset.filter(poll__org=self.request.org, is_active=True)
+        return queryset
+
+    def render_to_response(self, context, **kwargs):
+        segment = self.request.GET.get("segment", None)
+        if segment:
+            segment = json.loads(segment)
+
+        results = self.object.get_results(segment=segment)
+
+        return HttpResponse(json.dumps(results))
