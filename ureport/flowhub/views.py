148a3ae2a0f46d8f73b2d5d2e90fa471d1bfa6a0
basilio <eduardoafonsobasilio@gmail.com>
2019-10-11 17:09:10 -0300

    show delete and edit flow only in current org flows

--- ureport/flowhub/views.py
+++ ureport/flowhub/views.py
@@ -90,7 +90,7 @@ class CreateView(SmartCreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["page_subtitle"] = _("New")
+        context["subtitle"] = _("Upload New Flow")
         return context
 
 
