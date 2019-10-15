62f5d3de1be47f6b6b85b050c905a322cd955027
basilio <eduardoafonsobasilio@gmail.com>
2019-10-15 16:27:19 -0300

    created stars count

--- ureport/flowhub/migrations/0003_auto_20191015_0259.py
+++ ureport/flowhub/migrations/0003_auto_20191015_0259.py
@@ -6,14 +6,34 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('flowhub', '0002_auto_20191015_0257'),
-    ]
+    dependencies = [("flowhub", "0002_auto_20191015_0257")]
 
     operations = [
         migrations.AlterField(
-            model_name='flow',
-            name='languages',
-            field=django.contrib.postgres.fields.ArrayField(base_field=models.CharField(choices=[('bs', 'Bosnian'), ('en', 'English'), ('fr', 'French'), ('es', 'Spanish'), ('ar', 'Arabic'), ('pt', 'Portuguese'), ('pt-br', 'Brazilian Portuguese'), ('uk', 'Ukrainian'), ('uz', 'Uzbek'), ('my', 'Burmese'), ('id', 'Indonesian'), ('it', 'Italian'), ('ro', 'Romanian'), ('vi', 'Vietnamese'), ('sr-latn', 'Latin Serbian')], max_length=255), size=None),
-        ),
+            model_name="flow",
+            name="languages",
+            field=django.contrib.postgres.fields.ArrayField(
+                base_field=models.CharField(
+                    choices=[
+                        ("bs", "Bosnian"),
+                        ("en", "English"),
+                        ("fr", "French"),
+                        ("es", "Spanish"),
+                        ("ar", "Arabic"),
+                        ("pt", "Portuguese"),
+                        ("pt-br", "Brazilian Portuguese"),
+                        ("uk", "Ukrainian"),
+                        ("uz", "Uzbek"),
+                        ("my", "Burmese"),
+                        ("id", "Indonesian"),
+                        ("it", "Italian"),
+                        ("ro", "Romanian"),
+                        ("vi", "Vietnamese"),
+                        ("sr-latn", "Latin Serbian"),
+                    ],
+                    max_length=255,
+                ),
+                size=None,
+            ),
+        )
     ]
--- ureport/flowhub/urls.py
+++ ureport/flowhub/urls.py
@@ -6,7 +6,22 @@ urlpatterns = [
     url(r"^$", views.ListView.as_view(), name="flowhub.flow_list"),
     url(r"^my-org/$", views.MyOrgListView.as_view(), name="flowhub.my_org_flow_list"),
     url(r"^create/", views.CreateView.as_view(), name="flowhub.flow_create"),
-    url(r"^(?P<flow>[0-9]+)/download/$", views.DownloadView.as_view(), name="flowhub.flow_download"),
-    url(r"^(?P<flow>[0-9]+)/update/$", views.EditView.as_view(), name="flowhub.flow_update"),
-    url(r"^(?P<flow>[0-9]+)/delete/$", views.DeleteView.as_view(), name="flowhub.flow_delete"),
+    url(
+        r"^(?P<flow>[0-9]+)/download/$",
+        views.DownloadView.as_view(),
+        name="flowhub.flow_download",
+    ),
+    url(
+        r"^(?P<flow>[0-9]+)/stars/$", views.StarView.as_view(), name="flowhub.flow_star"
+    ),
+    url(
+        r"^(?P<flow>[0-9]+)/update/$",
+        views.EditView.as_view(),
+        name="flowhub.flow_update",
+    ),
+    url(
+        r"^(?P<flow>[0-9]+)/delete/$",
+        views.DeleteView.as_view(),
+        name="flowhub.flow_delete",
+    ),
 ]
