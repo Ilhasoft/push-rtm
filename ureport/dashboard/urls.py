ce65697ce7fff40b96c1941924aec52e992a3c02
basilio <eduardoafonsobasilio@gmail.com>
2019-10-21 11:16:41 -0300

    url dashboard.local

--- ureport/dashboard/urls.py
+++ ureport/dashboard/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r"^local$", views.Dashboard.Local.as_view(), name="local"),
+    url(r"^local/$", views.Dashboard.Local.as_view(), name="dashboard.local"),
+    url(r"^global/$", views.Dashboard.Global.as_view(), name="dashboard.global2"),
 ]
--- ureport/dashboard/views.py
+++ ureport/dashboard/views.py
@@ -4,6 +4,6 @@ from smartmin.views import SmartTemplateView
 class Dashboard:
     class Local(SmartTemplateView):
         template_name = "dashboard/local.html"
-    
+
     class Global(SmartTemplateView):
-        template_name = "#"
+        template_name = "dashboard/global.html"
