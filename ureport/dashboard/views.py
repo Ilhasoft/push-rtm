29019997cafafdfd3b5937ccb12fd1d5c9099d19
basilio <eduardoafonsobasilio@gmail.com>
2019-10-22 12:08:32 -0300

    bubble chart with real data

--- ureport/dashboard/views.py
+++ ureport/dashboard/views.py
@@ -1,13 +1,17 @@
 import json
 
+from django.conf import settings
+
 from smartmin.views import SmartTemplateView
+
+from ureport.polls.models import PollQuestion
 
 
 class Dashboard:
     class Local(SmartTemplateView):
         template_name = "dashboard/local.html"
 
-        def get_mock_data(self):
+        def get_bubble_chart_data_mock(self):
             """
             for test in templates
             """
@@ -18,10 +22,37 @@ class Dashboard:
                 fake_data[i+1] = {"x": values[0], "y":values[1], "r":values[2]}
             return fake_data
 
+        def get_bubble_chart_data(self, questions):
+            
+            # create dict with sdgs and yours questions. eg: {1: {'questions': []}}
+            sdgs_with_data = {sdg[0]: {'questions': [q for q in questions if sdg[0] in q.sdgs]} for sdg in settings.SDG_LIST}
+
+            # add keys total_responded and percentage_in_questions to sdgs_with_data
+            for key, value in sdgs_with_data.items():
+                if len(value['questions']) > 0:
+                    sdgs_with_data[key]['total_responded'] = value['questions'][0].get_responded()
+                    sdgs_with_data[key]['percentage_in_questions'] = (len(value['questions']) / questions.count()) * 100  # (part / total) * 100
+            
+            # implement chart.js bubble chart data model for all SDGs
+            data = {}
+
+            for key, value in settings.SDG_LIST:
+                sdg_with_data = sdgs_with_data.get(key)
+                
+                data["bubble-sdg-{}".format(key)] = {
+                    "x": sdg_with_data.get('total_responded', 0),
+                    "y": len(sdg_with_data.get('questions', [])),
+                    "r": sdg_with_data.get('percentage_in_questions', 0),
+                }
+            
+            return data
+
         def get_context_data(self, **kwargs):
             context = super().get_context_data(**kwargs)
-            data = self.get_mock_data()
-            context["bublle_data"] = data
+
+            questions = PollQuestion.objects.filter(poll__org=self.request.org)
+            context["bublle_data"] = self.get_bubble_chart_data(questions)
+            
             return context
 
     class Global(SmartTemplateView):
