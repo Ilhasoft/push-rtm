07fce58f3215a8874334187dc1138c8b9ec4f63d
basilio <eduardoafonsobasilio@gmail.com>
2019-10-23 18:07:00 -0300

    created sdgs tracked filters

--- ureport/dashboard/views.py
+++ ureport/dashboard/views.py
@@ -1,5 +1,6 @@
 import json
 import random
+import datetime
 
 from django.conf import settings
 
@@ -10,12 +11,12 @@ from ureport.polls.models import PollQuestion
 
 class Dashboard:
     @classmethod
-    def get_bubble_chart_data_sdgs_being_tracked(self, questions, mock=False):
-        '''
+    def get_sdgs_tracked_bubble_chart_data(self, questions, mock=False):
+        """
         return data to chart.js bubble chart.
         get questions queryset and boolean mock parammeter.
         default mock is False.
-        '''
+        """
         tracked_sdg = []
         not_tracked_sdg = []
         datasets = []
@@ -29,14 +30,14 @@ class Dashboard:
 
                     datasets.append(
                         {
-                            'label': '{} {}'.format(key, value),
-                            'data': [{"x": values[0], "y": values[1], "r": values[2]}],
-                            'backgroundColor': settings.SDG_COLOR.get(key),
-                            'borderColor': '#FFFFFF',
+                            "label": "{} {}".format(key, value),
+                            "data": [{"x": values[0], "y": values[1], "r": values[2]}],
+                            "backgroundColor": settings.SDG_COLOR.get(key),
+                            "borderColor": "#FFFFFF",
                         }
                     )
                 else:
-                    not_tracked_sdg.append(settings.SDG_LIST[key-1])
+                    not_tracked_sdg.append(settings.SDG_LIST[key - 1])
 
         else:  # USE REAL DATA
             # create dict with sdgs and yours questions. eg: {1: {'questions': []}}
@@ -54,9 +55,9 @@ class Dashboard:
                     sdgs_with_data[key]["percentage_in_questions"] = (
                         len(value["questions"]) / questions.count()
                     ) * 100  # (part / total) * 100
-                    tracked_sdg.append(settings.SDG_LIST[key-1])
+                    tracked_sdg.append(settings.SDG_LIST[key - 1])
                 else:
-                    not_tracked_sdg.append(settings.SDG_LIST[key-1])
+                    not_tracked_sdg.append(settings.SDG_LIST[key - 1])
 
             # implement chart.js bubblechart data.datasets model for all SDGs
             for key, value in tuple(tracked_sdg):
@@ -64,35 +65,72 @@ class Dashboard:
 
                 datasets.append(
                     {
-                        'label': '{} {}'.format(key, value),
-                        'data': [
+                        "label": "{} {}".format(key, value),
+                        "data": [
                             {
                                 "x": sdg_with_data.get("total_responded", 0),
                                 "y": len(sdg_with_data.get("questions", [])),
                                 "r": sdg_with_data.get("percentage_in_questions", 0),
                             }
                         ],
-                        'backgroundColor': settings.SDG_COLOR.get(key),
-                        'borderColor': '#FFFFFF',
+                        "backgroundColor": settings.SDG_COLOR.get(key),
+                        "borderColor": "#FFFFFF",
                     }
                 )
 
         data = {
-            'tracked_sdgs': tuple(tracked_sdg),
-            'not_tracked_sdgs': tuple(not_tracked_sdg),
-            'datasets': datasets,
+            "tracked_sdgs": tuple(tracked_sdg),
+            "not_tracked_sdgs": tuple(not_tracked_sdg),
+            "datasets": datasets,
         }
 
         return data
+
+    @classmethod
+    def questions_filter(self, questions, sorted_field):
+        one_year_ago = datetime.date.today() - datetime.timedelta(days=365)
+        one_moth_ago = datetime.date.today() - datetime.timedelta(days=30)
+        one_week_ago = datetime.date.today() - datetime.timedelta(days=7)
+
+        filters = {}
+
+        if sorted_field is None:
+            filters["created_on__gte"] = one_year_ago
+        elif sorted_field == "sdg_track_last_month":
+            filters["created_on__gte"] = one_moth_ago
+        elif sorted_field == "sdg_track_last_week":
+            filters["created_on__gte"] = one_week_ago
+
+        questions = questions.filter(**filters)
+
+        return questions
 
     class Local(SmartTemplateView):
         template_name = "dashboard/local.html"
 
         def get_context_data(self, **kwargs):
             context = super().get_context_data(**kwargs)
+            sorted_field = self.request.GET.get("sort")
 
-            questions = PollQuestion.objects.filter(is_active=True, poll__org=self.request.org, poll__is_active=True)
-            context["sdgs_bublle_data"] = Dashboard.get_bubble_chart_data_sdgs_being_tracked(questions)
+            ### SDG TRAKED BUBBLE CHART ###
+            sdg_tracked_questions = PollQuestion.objects.filter(
+                is_active=True, poll__org=self.request.org, poll__is_active=True
+            )
+
+            if sorted_field in [None, "sdg_track_last_month", "sdg_track_last_week"]:
+                sdg_tracked_questions = Dashboard.questions_filter(
+                    sdg_tracked_questions, sorted_field
+                )
+
+            context["sdgs_bubble_data"] = Dashboard.get_sdgs_tracked_bubble_chart_data(
+                sdg_tracked_questions
+            )
+
+            ### SURVEY PARTIAL RESULT CHART ###
+
+            ### MOST USED CHANNELS CHARTS ###
+
+            ### RAPIDPRO CONTACTS ###
 
             return context
 
