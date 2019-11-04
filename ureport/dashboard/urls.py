aed3b54b142b5e6620d598abff4d09ce3c44d8ae
basilio <eduardoafonsobasilio@gmail.com>
2019-11-04 12:18:57 -0300

    dashboard create only view and only template [wip]

--- ureport/dashboard/urls.py
+++ ureport/dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import Dashboard
+from . import views
 
 
 urlpatterns = [
-    url(r"^local/$", Dashboard.Local.as_view(), name="dashboard.local"),
-    url(r"^global/$", Dashboard.Global.as_view(), name="dashboard.global"),
+    #url(r"^local/$", views.Local.as_view(), name="dashboard.local"),
+    #url(r"^global/$", views.Global.as_view(), name="dashboard.global"),
+    url(r"^$", views.Dashboard.as_view(), name="dashboard"),
 ]
--- ureport/dashboard/views.py
+++ ureport/dashboard/views.py
@@ -16,7 +16,8 @@ from ureport.channels.models import ChannelStats, ChannelDailyStats
 from ureport.contacts.models import Contact
 
 
-class Dashboard:
+class Dashboard(SmartTemplateView):
+    template_name = "dashboard/dashboard.html"
 
     @classmethod
     def get_sdgs_tracked_bubble_chart_data(self, questions, mock=False):
@@ -191,212 +192,231 @@ class Dashboard:
     def channel_info(self, urn, field):
         return dict(settings.CHANNEL_TYPES).get(urn).get(field)
 
-    class Local(SmartTemplateView):
-        template_name = "dashboard/local.html"
 
-        def get(self, request, *args, **kwargs):
-            context = self.get_context_data(**kwargs)
-            if not self.request.user.is_authenticated:
-                return redirect(reverse("users.user_login"))
-            return self.render_to_response(context)
+    def get(self, request, *args, **kwargs):
+        # if not self.request.user.is_authenticated:
+        #     return redirect(reverse("users.user_login"))
+        if request.user.is_authenticated:
+            if request.user.is_superuser:
+                self.access_level = 'global'
+            elif request.org in request.user.get_user_orgs():
+                self.access_level = 'local'
+        else:
+            return redirect(reverse("users.user_login"))
+        
+        if self.access_level is None:
+            pass  # redirect to home
 
-        def get_context_data(self, **kwargs):
-            context = super().get_context_data(**kwargs)
-            channels_metrics_by = self.request.GET.get(
-                "message_metrics_by", "week")
-            channels_metrics_uuid = self.request.GET.get(
-                "message_metrics_uuid", "")
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
 
-            most_used_by = self.request.GET.get(
-                "most_used_by", "week")
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['access_level'] = self.access_level
 
-            questions = PollQuestion.objects.filter(
-                is_active=True, poll__org=self.request.org, poll__is_active=True
+        channels_metrics_by = self.request.GET.get(
+            "message_metrics_by", "week")
+        channels_metrics_uuid = self.request.GET.get(
+            "message_metrics_uuid", "")
+
+        most_used_by = self.request.GET.get(
+            "most_used_by", "week")
+
+        questions = PollQuestion.objects.filter(
+            is_active=True, poll__is_active=True
+        )
+
+        if self.access_level == 'local':
+            questions = questions.filter(poll__org=self.request.org)
+
+        # SDG TRAKED BUBBLE CHART ###
+        sdg_tracked_filter = self.request.GET.get("sdg_tracked_filter")
+        if sdg_tracked_filter is None:
+            sdg_tracked_filter = "year"
+
+        sdg_tracked_questions = questions
+
+        if sdg_tracked_filter in ["week", "month", "year"]:
+            sdg_tracked_questions = Dashboard.questions_filter(
+                questions, created_on=sdg_tracked_filter
             )
 
-            # SDG TRAKED BUBBLE CHART ###
-            sdg_tracked_filter = self.request.GET.get("sdg_tracked_filter")
-            if sdg_tracked_filter is None:
-                sdg_tracked_filter = "year"
+        context["sdg_tracked_filter"] = sdg_tracked_filter
+        context["sdgs_bubble_data"] = Dashboard.get_sdgs_tracked_bubble_chart_data(
+            sdg_tracked_questions
+        )
 
-            sdg_tracked_questions = questions
+        # SURVEY PARTIAL RESULT CHART ###
 
-            if sdg_tracked_filter in ["week", "month", "year"]:
-                sdg_tracked_questions = Dashboard.questions_filter(
-                    questions, created_on=sdg_tracked_filter
-                )
+        # filter only surveys opened
+        survey_result_sdg_questions = questions.filter(
+            poll__poll_end_date=datetime.datetime.utcnow().replace(tzinfo=utc)
+        )
 
-            context["sdg_tracked_filter"] = sdg_tracked_filter
-            context["sdgs_bubble_data"] = Dashboard.get_sdgs_tracked_bubble_chart_data(
-                sdg_tracked_questions
-            )
+        survey_result_sdg = self.request.GET.get("survey_result_sdg")
 
-            # SURVEY PARTIAL RESULT CHART ###
-
-            # filter only surveys opened
+        if survey_result_sdg is None:
+            survey_result_sdg_questions = questions
+        else:
+            survey_result_sdg = int(survey_result_sdg)
             survey_result_sdg_questions = questions.filter(
-                poll__poll_end_date=datetime.datetime.utcnow().replace(tzinfo=utc)
+                sdgs__contains=[survey_result_sdg]
             )
+            context["survey_result_sdg"] = settings.SDG_LIST[
+                survey_result_sdg - 1]
 
-            survey_result_sdg = self.request.GET.get("survey_result_sdg")
+        # show only question with data chart
+        survey_result_sdg_questions = [q for q in survey_result_sdg_questions if q.get_responded() > 0]
 
-            if survey_result_sdg is None:
-                survey_result_sdg_questions = questions
-            else:
-                survey_result_sdg = int(survey_result_sdg)
-                survey_result_sdg_questions = questions.filter(
-                    sdgs__contains=[survey_result_sdg]
-                )
-                context["survey_result_sdg"] = settings.SDG_LIST[
-                    survey_result_sdg - 1]
+        # shuffled questions
+        survey_result_sdg_questions = list(survey_result_sdg_questions)
+        random.shuffle(survey_result_sdg_questions)
 
-            # show only question with data chart
-            survey_result_sdg_questions = [q for q in survey_result_sdg_questions if q.get_responded() > 0]
-
-            # shuffled questions
-            survey_result_sdg_questions = list(survey_result_sdg_questions)
-            random.shuffle(survey_result_sdg_questions)
-
-            survey_result_choice_question = self.request.GET.get(
-                "survey_result_choice_question"
+        survey_result_choice_question = self.request.GET.get(
+            "survey_result_choice_question"
+        )
+        try:
+            survey_result_choice_question = PollQuestion.objects.get(
+                pk=survey_result_choice_question
             )
-            try:
-                survey_result_choice_question = PollQuestion.objects.get(
-                    pk=survey_result_choice_question
-                )
-            except PollQuestion.DoesNotExist:
-                survey_result_choice_question = None
+        except PollQuestion.DoesNotExist:
+            survey_result_choice_question = None
 
-            # max 20 questions
-            context["survey_result_sdg_questions"] = survey_result_sdg_questions[:20]
+        # max 20 questions
+        context["survey_result_sdg_questions"] = survey_result_sdg_questions[:20]
 
-            # MESSAGE METRICS
-            channels = ChannelStats.objects.filter(
-                org=self.request.org).order_by("channel_type")
+        # MESSAGE METRICS
+        channels = ChannelStats.objects.all().order_by("channel_type")
 
-            channels_info = {}
-            channels_data = {}
+        if self.access_level == 'local':
+            channels = channels.filter(org=self.request.org)
 
-            for channel in channels:
-                channels_info[channel.uuid] = {
-                    "name": Dashboard.channel_info(channel.channel_type, "name"),
-                    "icon": Dashboard.channel_info(channel.channel_type, "icon"),
-                }
-            context["channels_info"] = channels_info
+        channels_info = {}
+        channels_data = {}
 
-            for channel in channels:
-                total = ChannelDailyStats.objects.filter(
-                    channel=channel, **Dashboard.filter_by_date("date", channels_metrics_by)
-                ).aggregate(
-                    total=Sum("count")
-                )["total"]
+        for channel in channels:
+            channels_info[channel.uuid] = {
+                "name": Dashboard.channel_info(channel.channel_type, "name"),
+                "icon": Dashboard.channel_info(channel.channel_type, "icon"),
+            }
+        context["channels_info"] = channels_info
 
-                global_total = ChannelDailyStats.objects.exclude(
-                    channel__org=self.request.org,
-                ).filter(
-                    channel__channel_type=channel.channel_type,
-                    **Dashboard.filter_by_date("date", channels_metrics_by)
-                ).aggregate(
-                    total=Sum("count")
-                )["total"]
-
-                channels_data[channel.uuid] = {
-                    "name": Dashboard.channel_info(channel.channel_type, "name"),
-                    "icon": Dashboard.channel_info(channel.channel_type, "icon"),
-                    "total": total if total is not None else 0,
-                    "global": global_total if global_total is not None else 0,
-                }
-
-            if channels_metrics_uuid:
-                channels = channels.filter(uuid=channels_metrics_uuid)
-
-            channels_chart_stats = ChannelDailyStats.objects.filter(
-                channel__org=self.request.org,
-                channel__in=channels,
-                msg_direction__in=["I", "O", "E"],
-                msg_type__in=["M", "I", "E"],
-                **Dashboard.filter_by_date("date", channels_metrics_by),
-            ).order_by("date")
-
-            context["channels_chart_stats"] = channels_chart_stats
-            context["channels_data"] = channels_data
-            context["channels_metrics_uuid"] = channels_metrics_uuid
-            context["channels_metrics_by"] = channels_metrics_by
-            context["channels_messages_ago"] = Dashboard.get_text_time_ago(
-                channels_metrics_by)
-
-            # MOST USED CHANNELS CHARTS
-            context["surveys_total"] = Poll.objects.filter(
-                org=self.request.org, is_active=True).count()
-
-            most_used = ChannelDailyStats.objects.filter(
-                channel__org=self.request.org,
-                msg_direction__in=["I", "O"],
-                msg_type__in=["M", "I"],
-                **Dashboard.filter_by_date("date", most_used_by),
-            ).values(
-                "channel__channel_type"
-            ).annotate(
+        for channel in channels:
+            total = ChannelDailyStats.objects.filter(
+                channel=channel, **Dashboard.filter_by_date("date", channels_metrics_by)
+            ).aggregate(
                 total=Sum("count")
-            ).order_by("-total")[:3]
+            )["total"]
 
-            most_used_global = ChannelDailyStats.objects.exclude(
+            global_total = ChannelDailyStats.objects.exclude(
                 channel__org=self.request.org,
             ).filter(
-                msg_direction__in=["I", "O"],
-                msg_type__in=["M", "I"],
-                **Dashboard.filter_by_date("date", most_used_by),
-            ).values(
-                "channel__channel_type"
-            ).annotate(
+                channel__channel_type=channel.channel_type,
+                **Dashboard.filter_by_date("date", channels_metrics_by)
+            ).aggregate(
                 total=Sum("count")
-            ).order_by("-total")[:3]
+            )["total"]
 
-            channels_most_used = []
-            channels_most_used_global = []
-
-            for channel in most_used:
-                channels_most_used.append(
-                    {
-                        "name": Dashboard.channel_info(channel.get("channel__channel_type"), "name"),
-                        "total": channel.get("total", 0),
-                    }
-                )
-
-            for channel in most_used_global:
-                channels_most_used_global.append(
-                    {
-                        "name": Dashboard.channel_info(channel.get("channel__channel_type"), "name"),
-                        "total": channel.get("total", 0),
-                    }
-                )
-
-            context["channels_most_used_ago"] = Dashboard.get_text_time_ago(
-                most_used_by)
-            context["channels_most_used"] = channels_most_used
-            context["channels_most_used_global"] = channels_most_used_global
-
-            # RAPIDPRO CONTACTS
-            context["contacts_over_time"] = Contact.objects.filter(
-                registered_on__gte=datetime.datetime.utcnow().replace(tzinfo=utc) - datetime.timedelta(days=180),
-            ).annotate(
-                month=ExtractMonth("registered_on"),
-                year=ExtractYear("registered_on")).order_by("month").values(
-                "month",
-                "year"
-            ).annotate(total=Count("*")).values(
-                "month",
-                "year",
-                "total",
-                "org",
-            )
-
-            context["global_total_contacts"] = {
-                "local": Contact.objects.filter(org=self.request.org).count(),
-                "global": Contact.objects.exclude(org=self.request.org).count(),
+            channels_data[channel.uuid] = {
+                "name": Dashboard.channel_info(channel.channel_type, "name"),
+                "icon": Dashboard.channel_info(channel.channel_type, "icon"),
+                "total": total if total is not None else 0,
+                "global": global_total if global_total is not None else 0,
             }
 
-            return context
+        if channels_metrics_uuid:
+            channels = channels.filter(uuid=channels_metrics_uuid)
 
-    class Global(SmartTemplateView):
-        template_name = "dashboard/global.html"
+        channels_chart_stats = ChannelDailyStats.objects.filter(
+            #channel__org=self.request.org,
+            channel__in=channels,
+            msg_direction__in=["I", "O", "E"],
+            msg_type__in=["M", "I", "E"],
+            **Dashboard.filter_by_date("date", channels_metrics_by),
+        ).order_by("date")
+
+        if self.access_level == 'local':
+            channels_chart_stats = channels_chart_stats.filter(channel__org=self.request.org)
+
+        context["channels_chart_stats"] = channels_chart_stats
+        context["channels_data"] = channels_data
+        context["channels_metrics_uuid"] = channels_metrics_uuid
+        context["channels_metrics_by"] = channels_metrics_by
+        context["channels_messages_ago"] = Dashboard.get_text_time_ago(
+            channels_metrics_by)
+
+        # MOST USED CHANNELS CHARTS
+        context["surveys_total"] = Poll.objects.filter(
+            org=self.request.org, is_active=True).count()
+
+        most_used = ChannelDailyStats.objects.filter(
+            #channel__org=self.request.org,
+            msg_direction__in=["I", "O"],
+            msg_type__in=["M", "I"],
+            **Dashboard.filter_by_date("date", most_used_by),
+        )
+        
+        if self.access_level == 'local':
+            most_used = most_used.filter(
+                channel__org=self.request.org,
+            )
+        
+        most_used = most_used.filter().values("channel__channel_type").annotate(total=Sum("count")).order_by("-total")[:3]
+
+        most_used_global = ChannelDailyStats.objects.exclude(
+            channel__org=self.request.org,
+        ).filter(
+            msg_direction__in=["I", "O"],
+            msg_type__in=["M", "I"],
+            **Dashboard.filter_by_date("date", most_used_by),
+        ).values(
+            "channel__channel_type"
+        ).annotate(
+            total=Sum("count")
+        ).order_by("-total")[:3]
+
+        channels_most_used = []
+        channels_most_used_global = []
+
+        for channel in most_used:
+            channels_most_used.append(
+                {
+                    "name": Dashboard.channel_info(channel.get("channel__channel_type"), "name"),
+                    "total": channel.get("total", 0),
+                }
+            )
+
+        for channel in most_used_global:
+            channels_most_used_global.append(
+                {
+                    "name": Dashboard.channel_info(channel.get("channel__channel_type"), "name"),
+                    "total": channel.get("total", 0),
+                }
+            )
+
+        context["channels_most_used_ago"] = Dashboard.get_text_time_ago(
+            most_used_by)
+        context["channels_most_used"] = channels_most_used
+        context["channels_most_used_global"] = channels_most_used_global
+
+        # RAPIDPRO CONTACTS
+        context["contacts_over_time"] = Contact.objects.filter(
+            registered_on__gte=datetime.datetime.utcnow().replace(tzinfo=utc) - datetime.timedelta(days=180),
+        ).annotate(
+            month=ExtractMonth("registered_on"),
+            year=ExtractYear("registered_on")).order_by("month").values(
+            "month",
+            "year"
+        ).annotate(total=Count("*")).values(
+            "month",
+            "year",
+            "total",
+            "org",
+        )
+
+        context["global_total_contacts"] = {
+            "local": Contact.objects.filter(org=self.request.org).count(),
+            "global": Contact.objects.exclude(org=self.request.org).count(),
+        }
+
+        return context
