8bdf7b80920248851c53ba7cea54f4263f8d94ab
Eduardo Douglas <edudouglas@gmail.com>
2019-11-19 16:13:55 -0300

    Initial dashboard api

--- ureport/api/urls.py
+++ ureport/api/urls.py
@@ -7,19 +7,11 @@ from django.conf.urls import url
 from django.views.generic import RedirectView
 
 from ureport.api.views import (
-    FeaturedPollList,
-    ImageDetails,
-    ImageList,
-    NewsItemDetails,
-    NewsItemList,
     OrgDetails,
     OrgList,
     PollDetails,
     PollList,
-    StoryDetails,
-    StoryList,
-    VideoDetails,
-    VideoList,
+    DashboardDetails,
 )
 
 schema_view = get_swagger_view(title="API")
@@ -31,14 +23,6 @@ urlpatterns = [
     url(r"^orgs/$", OrgList.as_view(), name="api.v1.org_list"),
     url(r"^orgs/(?P<pk>[\w]+)/$", OrgDetails.as_view(), name="api.v1.org_details"),
     url(r"^polls/org/(?P<org>[\w]+)/$", PollList.as_view(), name="api.v1.org_poll_list"),
-    url(r"^polls/org/(?P<org>[\w]+)/featured/$", FeaturedPollList.as_view(), name="api.v1.org_poll_fetured"),
     url(r"^polls/(?P<pk>[\w]+)/$", PollDetails.as_view(), name="api.v1.poll_details"),
-    url(r"^news/org/(?P<org>[\w]+)/$", NewsItemList.as_view(), name="api.v1.org_newsitem_list"),
-    url(r"^news/(?P<pk>[\w]+)/$", NewsItemDetails.as_view(), name="api.v1.newsitem_details"),
-    url(r"^videos/org/(?P<org>[\w]+)/$", VideoList.as_view(), name="api.v1.org_video_list"),
-    url(r"^videos/(?P<pk>[\w]+)/$", VideoDetails.as_view(), name="api.v1.video_details"),
-    url(r"^assets/org/(?P<org>[\w]+)/$", ImageList.as_view(), name="api.v1.org_asset_list"),
-    url(r"^assets/(?P<pk>[\w]+)/$", ImageDetails.as_view(), name="api.v1.asset_details"),
-    url(r"^stories/org/(?P<org>[\w]+)/$", StoryList.as_view(), name="api.v1.org_story_list"),
-    url(r"^stories/(?P<pk>[\w]+)/$", StoryDetails.as_view(), name="api.v1.story_details"),
+    url(r"^dashboard/(?P<pk>[\w]+)/$", DashboardDetails.as_view(), name="api.v1.dashboard_details"),
 ]
