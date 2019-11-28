fe6f6d9ed5f65682fc798077fed20a3f0ed5a0e3
Eduardo Douglas <edudouglas@gmail.com>
2019-11-28 10:16:31 -0300

    Fix json error

--- ureport/authentication/views.py
+++ ureport/authentication/views.py
@@ -10,76 +10,82 @@ from django.conf import settings
 from django.contrib.auth import get_user_model, login
 from django.contrib.auth.models import Group
 from django.shortcuts import redirect, reverse
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
 
 
 class LoginAuthView(SmartTemplateView):
     template_name = "authentication/login.html"
 
     def get(self, request, *args, **kwargs):
-        code = self.request.GET.get("code")
-        org = self.request.org
+        try:
+            code = self.request.GET.get("code")
+            org = self.request.org
 
-        context = self.get_context_data(**kwargs)
-        context["org"] = org
+            context = self.get_context_data(**kwargs)
+            context["org"] = org
 
-        oauth = OAuth2Session(
-            settings.OAUTHLIB_CLIENT_ID,
-            redirect_uri=settings.OAUTHLIB_REDIRECT_URI,
-            scope="openid email profile",
-            state=str(base64.b64encode(org.subdomain.encode("utf-8")), "utf-8") if hasattr(org, "subdomain") else None,
-        )
-
-        if code:
-            fetch_token = oauth.fetch_token(
-                settings.OAUTHLIB_TOKEN_URL,
-                code=code,
-                include_client_id=settings.OAUTHLIB_CLIENT_ID,
-                client_secret=settings.OAUTHLIB_SECRET,
-                auth="",
+            oauth = OAuth2Session(
+                settings.OAUTHLIB_CLIENT_ID,
+                redirect_uri=settings.OAUTHLIB_REDIRECT_URI,
+                scope="openid email profile",
+                state=str(base64.b64encode(org.subdomain.encode("utf-8")), "utf-8") if hasattr(org, "subdomain") else None,
             )
 
-            token = fetch_token.get("access_token")
-            session = OAuth2Session(client_id=settings.OAUTHLIB_CLIENT_ID, token=token)
-            session.access_token = token
-            user_data = session.get(url=settings.OAUTHLIB_USER_URL).json()
+            if code:
+                fetch_token = oauth.fetch_token(
+                    settings.OAUTHLIB_TOKEN_URL,
+                    code=code,
+                    include_client_id=settings.OAUTHLIB_CLIENT_ID,
+                    client_secret=settings.OAUTHLIB_SECRET,
+                    auth="",
+                )
 
-            response = requests.post(
-                settings.OAUTHLIB_MOMSERVICE_TOKEN_URL,
-                data={"app_secret": settings.OAUTHLIB_SECRET, "app_id": settings.OAUTHLIB_APP_ID},
-            )
+                token = fetch_token.get("access_token")
+                session = OAuth2Session(client_id=settings.OAUTHLIB_CLIENT_ID, token=token)
+                session.access_token = token
+                user_data = session.get(url=settings.OAUTHLIB_USER_URL).json()
 
-            response = requests.post(
-                settings.OAUTHLIB_MOMSERVICE_USER_URL,
-                data={"username": user_data.get("preferred_username")},
-                headers={"authorizationtoken": response.json().get("data").get("token")},
-            )
+                response = requests.post(
+                    settings.OAUTHLIB_MOMSERVICE_TOKEN_URL,
+                    data={"app_secret": settings.OAUTHLIB_SECRET, "app_id": settings.OAUTHLIB_APP_ID},
+                )
 
-            extra_data = json.loads(response.text).get("data")
-            workspace = extra_data.get("workspace")
+                response = requests.post(
+                    settings.OAUTHLIB_MOMSERVICE_USER_URL,
+                    data={"username": user_data.get("preferred_username")},
+                    headers={"authorizationtoken": response.json().get("data").get("token")},
+                )
 
-            if self.request.org.subdomain in workspace:
-                try:
-                    user = get_user_model().objects.get(
-                        email=extra_data.get("email"), username=extra_data.get("userid")
-                    )
-                except get_user_model().DoesNotExist:
-                    user = get_user_model().objects.create(
-                        username=extra_data.get("userid"),
-                        first_name=extra_data.get("username"),
-                        email=extra_data.get("email"),
-                    )
-                    user.set_password(random.getrandbits(128))
-                    user.save()
+                extra_data = json.loads(response.text).get("data")
+                workspace = extra_data.get("workspace")
 
-                    group = Group.objects.get(name="Viewers")
-                    user.groups.add(group)
-                    self.request.org.viewers.add(user)
+                if self.request.org.subdomain in workspace:
+                    try:
+                        user = get_user_model().objects.get(
+                            email=extra_data.get("email"), username=extra_data.get("userid")
+                        )
+                    except get_user_model().DoesNotExist:
+                        user = get_user_model().objects.create(
+                            username=extra_data.get("userid"),
+                            first_name=extra_data.get("username"),
+                            email=extra_data.get("email"),
+                        )
+                        user.set_password(random.getrandbits(128))
+                        user.save()
 
-                login(self.request, user)
-                return redirect(reverse("dashboard"))
-        else:
-            authorization_url, state = oauth.authorization_url(settings.OAUTHLIB_AUTHORIZATION_URL)
-            context["authorization_url"] = authorization_url
+                        group = Group.objects.get(name="Viewers")
+                        user.groups.add(group)
+                        self.request.org.viewers.add(user)
+
+                    login(self.request, user)
+                    return redirect(reverse("dashboard"))
+            else:
+                authorization_url, state = oauth.authorization_url(settings.OAUTHLIB_AUTHORIZATION_URL)
+                context["authorization_url"] = authorization_url
+        except Exception:
+            messages.error(self.request, _("Please try again."))
+            return redirect(reverse("authentication.login"))
 
         return self.render_to_response(context)
 
