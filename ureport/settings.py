e0eb4faec2fc17f27db02f40a2019d55e25fbfe7
Eduardo Douglas <edudouglas@gmail.com>
2019-12-04 10:08:30 -0300

    Redirect logout

--- ureport/settings.py
+++ ureport/settings.py
@@ -179,7 +179,7 @@ SASS_PROCESSOR_INCLUDE_FILE_PATTERN = r"^.+\.scss$"
 LOGIN_URL = "/users/login/"
 LOGOUT_URL = "/users/logout/"
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "http://dataforallcloud.org/"
 
 SITE_CHOOSER_TEMPLATE = "public/index.html"
 SITE_CHOOSER_URL_NAME = "public.index"
