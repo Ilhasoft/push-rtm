b7b04cd81a3d7e3896c2b24f359a996121f9397f
Jonathan Mieldazis <mieldazis@ilhasoft.com.br>
2020-01-09 14:17:50 -0300

    finally refactore review PR

--- ureport/results/views.py
+++ ureport/results/views.py
@@ -106,11 +106,43 @@ class PollGlobalDataView(View):
         self._global_questions = {}
         self._local_questions = []
 
+    def _get_question_results_without_segment(self, question):
+        """ Get question data without segment. """
+        statistics = {}
+        word_cloud = []
+        if question.is_open_ended():
+            for category in question.get_results()[0].get("categories"):
+                count = category.get("count")
+                word_cloud.append(
+                    {
+                        "text": category.get("label").upper(),
+                        "size": count if count > 10 else 20 + count
+                    }
+                )
+        else:
+            labels = []
+            series = []
+            counts = []
+
+            results = question.get_results()[0]
+            categories = results.get("categories")
+            for category in categories:
+                labels.append(category.get("label"))
+                series.append("{0:.0f}".format(category.get("count") / results.get("set") * 100))
+                counts.append(category.get("count"))
+
+            statistics["labels"] = labels
+            statistics["series"] = series
+            statistics["counts"] = counts
+
+        dict_result = {
+            "statistics": statistics,
+            "word_cloud": word_cloud
+        }
+        return dict_result
+
     def _get_question_segmented_results(self, question, tag):
-        """
-        Get question data sorted by tag.
-        Tag's example: All, Age, Gender.
-        """
+        """ Get question data sorted by tag. Tag's example: Age, Gender. """
         return question_segmented_results(question, tag)
 
     def _get_formated_question_segmented_results(self, question, tag):
@@ -149,17 +181,18 @@ class PollGlobalDataView(View):
         return dict_result
 
     def _sum_values_arrays(self, array_one, array_two):
-        """Sum values ​​from two arrays."""
+        """ Sum values ​​from two arrays. """
         return list(map(lambda x, y: x + y, array_one, array_two))
 
     def _generate_global_data(self, question, tag):
-        """Get data from a specific question and add to the overall results."""
+        """ Get data from a specific question and add to the overall results. """
         question_dict = self._global_questions.get(question.ruleset_label)
         values = question_dict.get(tag).get('series')
         values_to_return = []
         for value in values:
             current_value = value.get('data')
-            loop_value = self._get_formated_question_segmented_results(question, tag).get('series')[values.index(value)].get(
+            loop_value = self._get_formated_question_segmented_results(question, tag).get('series')[
+                values.index(value)].get(
                 'data')
             new_values_in_array = self._sum_values_arrays(current_value, loop_value)
             values_to_return.append(new_values_in_array)
@@ -167,12 +200,37 @@ class PollGlobalDataView(View):
         return values_to_return
 
     def _update_global_data(self, question, tag):
-        """Get the data of a question added to the global ones and update data global."""
+        """ Get the data of a question added to the global ones and update data global. """
         new_values = self._generate_global_data(question, tag)
         for values in new_values:
             self._global_questions[question.ruleset_label][tag]['series'][new_values.index(values)]['data'] = values
 
         return self._global_questions
+
+    def _get_results_in_dict(self, question):
+        """ Returns a final dictionary with all necessary information in the template. """
+        data_without_segment = self._get_question_results_without_segment(question)
+        result_dict = {
+            "id": question.pk,
+            "is_active": question.is_active,
+            "created_on": question.created_on,
+            "modified_on": question.modified_on,
+            "title": question.title,
+            "created_by_id": question.created_by_id,
+            "modified_by_id": question.modified_by_id,
+            "poll_id": question.poll_id,
+            "ruleset_label": question.ruleset_label,
+            "url": reverse("results.poll_read", args=[question.poll.pk]),
+            "is_open_ended": question.is_open_ended(),
+            "word_cloud": data_without_segment.get("word_cloud"),
+            "get_responded": question.get_responded(),
+            "get_polled": question.get_polled(),
+            "sdgs": question.sdgs,
+            "statistics": data_without_segment.get("statistics"),
+            "age": self._get_formated_question_segmented_results(question, "age"),
+            "gender": self._get_formated_question_segmented_results(question, "gender")
+        }
+        return result_dict
 
     def get(self, request, *args, **kwargs):
         global_survey = self.kwargs["pk"]
@@ -191,130 +249,32 @@ class PollGlobalDataView(View):
             poll_id__in=list(global_poll_local_id),
             poll__is_active=True
         )
-
-        response = dict()
-
         for question in global_polls_questions:
-            formated_segmented_results_age = self._get_formated_question_segmented_results(question, "age")
-            formated_segmented_results_gender = self._get_formated_question_segmented_results(question, "gender")
-
-            statistics = {}
-            word_cloud = []
-            if question.is_open_ended():
-                for category in question.get_results()[0].get("categories"):
-                    count = category.get("count")
-                    word_cloud.append(
-                        {
-                            "text": category.get("label").upper(),
-                            "size": count if count > 10 else 20 + count
-                        }
-                    )
-            else:
-                labels = []
-                series = []
-                counts = []
-
-                results = question.get_results()[0]
-                categories = results.get("categories")
-                for category in categories:
-                    labels.append(category.get("label"))
-                    series.append("{0:.0f}".format(category.get("count") / results.get("set") * 100))
-                    counts.append(category.get("count"))
-
-                statistics["labels"] = labels
-                statistics["series"] = series
-                statistics["counts"] = counts
-
             question_dict = self._global_questions.get(question.ruleset_label)
-
             if question_dict:
+                value_current_question_array = self._get_question_results_without_segment(question).get(
+                    "statistics").get("counts")
                 values_in_array_statistics = question_dict.get('statistics').get('counts')
-                new_values_in_array_statistics = self._sum_values_arrays(values_in_array_statistics, counts)
-                self._global_questions[question.ruleset_label]['statistics']['counts'] = new_values_in_array_statistics
 
+                new_values_in_array_statistics = self._sum_values_arrays(values_in_array_statistics,
+                                                                         value_current_question_array)
+                self._global_questions[question.ruleset_label]['statistics']['counts'] = new_values_in_array_statistics
                 self._update_global_data(question, 'age')
                 self._update_global_data(question, 'gender')
-
             else:
-                self._global_questions[question.ruleset_label] = ({
-                    "id": question.pk,
-                    "is_active": question.is_active,
-                    "created_on": question.created_on,
-                    "modified_on": question.modified_on,
-                    "title": question.title,
-                    "created_by_id": question.created_by_id,
-                    "modified_by_id": question.modified_by_id,
-                    "poll_id": question.poll_id,
-                    "ruleset_label": question.ruleset_label,
-                    "url": reverse("results.poll_read", args=[question.poll.pk]),
-                    "is_open_ended": question.is_open_ended(),
-                    "word_cloud": word_cloud,
-                    "get_responded": question.get_responded(),
-                    "get_polled": question.get_polled(),
-                    "sdgs": question.sdgs,
-                    "statistics": statistics,
-                    "age": formated_segmented_results_age,
-                    "gender": formated_segmented_results_gender
-                })
+                self._global_questions[question.ruleset_label] = (self._get_results_in_dict(question))
 
         local_poll_questions = PollQuestion.objects.filter(is_active=True, poll_id__in=unct, poll__is_active=True)
         local_poll_title = local_poll_questions[0].poll.title if local_poll_questions else None
-
         for question in local_poll_questions:
-            formated_segmented_results_age = self._get_formated_question_segmented_results(question, "age")
-            formated_segmented_results_gender = self._get_formated_question_segmented_results(question, "gender")
+            local_results = self._get_results_in_dict(question)
+            local_results["local_poll_title"] = local_poll_title
 
-            statistics = {}
-            word_cloud = []
-            if question.is_open_ended():
-                for category in question.get_results()[0].get("categories"):
-                    count = category.get("count")
-                    word_cloud.append(
-                        {"text": category.get("label").upper(), "size": count if count > 10 else 20 + count}
-                    )
-            else:
-                labels = []
-                series = []
-                counts = []
+            self._local_questions.append(local_results)
 
-                results = question.get_results()[0]
-                categories = results.get("categories")
-                for category in categories:
-                    labels.append(category.get("label"))
-                    series.append("{0:.0f}".format(category.get("count") / results.get("set") * 100))
-                    counts.append(category.get("count"))
+        self._response['questions_global'] = self._global_questions
+        self._response['questions_local'] = self._local_questions
+        self._response['number_questions'] = len(self._local_questions)
+        self._response['sdgs'] = dict(settings.SDG_LIST)
 
-                statistics["labels"] = labels
-                statistics["series"] = series
-                statistics["counts"] = counts
-
-            self._local_questions.append(
-                {
-                    "id": question.pk,
-                    "is_active": question.is_active,
-                    "created_on": question.created_on,
-                    "modified_on": question.modified_on,
-                    "title": question.title,
-                    "created_by_id": question.created_by_id,
-                    "modified_by_id": question.modified_by_id,
-                    "poll_id": question.poll_id,
-                    "ruleset_label": question.ruleset_label,
-                    "url": reverse("results.poll_read", args=[question.poll.pk]),
-                    "is_open_ended": question.is_open_ended(),
-                    "word_cloud": word_cloud,
-                    "get_responded": question.get_responded(),
-                    "get_polled": question.get_polled(),
-                    "sdgs": question.sdgs,
-                    "statistics": statistics,
-                    "age": formated_segmented_results_age,
-                    "gender": formated_segmented_results_gender,
-                    "local_poll_title": local_poll_title,
-                }
-            )
-
-        response['questions_global'] = self._global_questions
-        response['questions_local'] = self._local_questions
-        response['number_questions'] = len(self._local_questions)
-        response['sdgs'] = dict(settings.SDG_LIST)
-
-        return JsonResponse(response)
+        return JsonResponse(self._response)
