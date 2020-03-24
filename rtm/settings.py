b42673ffe3ff3ba47bfb2dbbba2a2920ed576f68
Jonathan Mieldazis <mieldazis@ilhasoft.com.br>
2020-03-24 12:11:34 -0300

    alter some docs like docker

--- rtm/settings.py
+++ rtm/settings.py
@@ -10,7 +10,7 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 TEMPLATE_DEBUG = DEBUG
 
 EMPTY_SUBDOMAIN_HOST = config("EMPTY_SUBDOMAIN_HOST", "http://localhost:8000")
-HOSTNAME = config("APP_HOSTNAME", "localhost:8000")
+HOSTNAME = config("HOSTNAME", "localhost:8000")
 SITE_HOST_PATTERN = config("SITE_HOST_PATTERN", default="http://{}.localhost:8000")
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*", cast=lambda v: [s.strip() for s in v.split(",")])
