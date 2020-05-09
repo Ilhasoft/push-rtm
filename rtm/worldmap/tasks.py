1855a53fffdc2c1c6d86cf26251641860be67861
Jonathan Mieldazis <mieldazis@ilhasoft.com.br>
2020-05-09 11:41:09 -0300

    refactore task create org country

--- rtm/worldmap/tasks.py
+++ rtm/worldmap/tasks.py
@@ -1,26 +1,27 @@
 from dash.orgs.models import Org
+from temba_client.v2 import TembaClient
+
 from rtm.celery import app
 
 from .models import OrgCountryCode
 
 
 @app.task()
-def create_org_country_code(*args, **kwargs):
-    org_id = kwargs.get("org_id")
-    if org_id:
-        org = Org.objects.get(pk=org_id)
-        temba_cliente = org.get_temba_client()
-        if temba_cliente:
-            data_org = temba_cliente.get_org()
-            if data_org:
-                code = data_org.country
-                if code:
-                    exist = OrgCountryCode.objects.filter(org_country_code=code).exists()
+def create_org_country_code(*args, **kwargs) -> bool:
+    try:
+        org_id: int = kwargs.get("org_id")
+        org: Org = Org.objects.get(pk=org_id)
 
-                    if not exist:
-                        org_country_code = OrgCountryCode.objects.create(org_country_code=code, org=org)
+        already_exist: bool = OrgCountryCode.objects.filter(org=org).exists()
+        if already_exist:
+            return True
 
-                    if not org_country_code:
-                        raise Exception
+        temba_cliente: TembaClient = org.get_temba_client()
+        temba_org: Org = temba_cliente.get_org()
+        code: str = temba_org.country
+        OrgCountryCode.objects.create(org_country_code=code, org=org)
+
+    except Exception as e:
+        pass
 
     return True
