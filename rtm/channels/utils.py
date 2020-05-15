fd137321a0808eef0a5fd32992ef2c3377783520
Jonathan Mieldazis <mieldazis@ilhasoft.com.br>
2020-05-15 12:26:06 -0300

    change worlmap return data. add pagination uncts

--- rtm/channels/utils.py
+++ rtm/channels/utils.py
@@ -6,15 +6,15 @@ from rtm.channels.models import ChannelDailyStats
 
 def get_messages_sent_org(org: Org) -> int:
     sent = ChannelDailyStats.objects.filter(msg_direction="O", channel__org=org).aggregate(total=Sum("count"))
-    return sent.get("total", 0)
+    sent = sent.get("total", 0)
+    return 0 if sent is None else sent
 
 
 def get_messages_received_org(org: Org) -> int:
     received = ChannelDailyStats.objects.filter(msg_direction="I", channel__org=org).aggregate(total=Sum("count"))
-    return received.get("total", 0)
+    received = received.get("total", 0)
+    return 0 if received is None else received
 
 
 def get_messages_engagement_org(sent: int = 0, received: int = 0) -> int:
-    sent = 0 if sent is None else sent
-    received = 0 if received is None else received
     return (100 * received) / sent if sent > 0 else 0
